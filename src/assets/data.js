5193f0d2da06431ff22becd0a9c8f11a7454519d
Diki Armanda <diki.armada5@gmail.com>
2024-06-14 19:48:35 +0700

    db: update  db

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,6 +1,6 @@
 export const data = [
   {
-      "Submitted at": "2024-06-02 08:05:09",
+      "Submitted at": "6/2/2024 8:05",
       "title": "JOIN",
       "longTitle": "Jurnal Online Informatika",
       "sinta": "SINTA 2",
@@ -9,52 +9,52 @@ export const data = [
       "urlJournal": "https://join.if.uinsgd.ac.id/index.php/join"
   },
   {
-      "Submitted at": "2024-06-02 08:06:20",
+      "Submitted at": "6/2/2024 8:06",
       "title": "JEPIN",
-      "longTitle": "Jurnal Edukasi dan Penelitian Informatika",
+      "longTitle": "Jurnal Edukasi Dan Penelitian Informatika",
       "sinta": "SINTA 2",
       "program": "INFORMATIKA",
       "fee": 1000000,
       "urlJournal": "https://jurnal.untan.ac.id/index.php/jepin/index"
   },
   {
-      "Submitted at": "2024-06-02 08:06:54",
+      "Submitted at": "6/2/2024 8:06",
       "title": "JTIIK",
-      "longTitle": "Jurnal Teknologi Informasi dan Ilmu Komputer",
+      "longTitle": "Jurnal Teknologi Informasi Dan Ilmu Komputer",
       "sinta": "SINTA 2",
       "program": "INFORMATIKA",
       "fee": 1000000,
       "urlJournal": "https://jtiik.ub.ac.id/index.php/jtiik"
   },
   {
-      "Submitted at": "2024-06-02 08:07:35",
+      "Submitted at": "6/2/2024 8:07",
       "title": "RESTI",
-      "longTitle": "Rekayasa Sistem dan Teknologi Informasi",
+      "longTitle": "Rekayasa Sistem Dan Teknologi Informasi",
       "sinta": "SINTA 2",
       "program": "INFORMATIKA",
       "fee": 2500000,
       "urlJournal": "https://jurnal.iaii.or.id/index.php/RESTI"
   },
   {
-      "Submitted at": "2024-06-02 08:08:01",
+      "Submitted at": "6/2/2024 8:08",
       "title": "JUSTIN",
-      "longTitle": "Jurnal Sistem dan Teknologi Informasi",
+      "longTitle": "Jurnal Sistem Dan Teknologi Informasi",
       "sinta": "SINTA 3",
       "program": "INFORMATIKA",
       "fee": 0,
       "urlJournal": "https://jurnal.untan.ac.id/index.php/justin/index"
   },
   {
-      "Submitted at": "2024-06-02 08:08:34",
+      "Submitted at": "6/2/2024 8:08",
       "title": "INFOTEK",
-      "longTitle": "Jurnal Informatika dan Teknologi",
+      "longTitle": "Jurnal Informatika Dan Teknologi",
       "sinta": "SINTA 4",
       "program": "INFORMATIKA",
       "fee": 400000,
       "urlJournal": "https://e-journal.hamzanwadi.ac.id/index.php/infotek/index"
   },
   {
-      "Submitted at": "2024-06-02 08:09:40",
+      "Submitted at": "6/2/2024 8:09",
       "title": "JUTIKOMP",
       "longTitle": "Jurnal Teknologi Dan Ilmu Komputer Prima",
       "sinta": "SINTA 5",
@@ -63,34 +63,34 @@ export const data = [
       "urlJournal": "https://jurnal.unprimdn.ac.id/index.php/JUTIKOMP/index"
   },
   {
-      "Submitted at": "2024-06-02 08:10:02",
+      "Submitted at": "6/2/2024 8:10",
       "title": "JATISI",
-      "longTitle": "Jurnal Teknik Informatika dan Sistem Informasi",
+      "longTitle": "Jurnal Teknik Informatika Dan Sistem Informasi",
       "sinta": "SINTA 5",
       "program": "INFORMATIKA",
       "fee": 0,
       "urlJournal": "https://jurnal.mdp.ac.id/index.php/jatisi/index"
   },
   {
-      "Submitted at": "2024-06-03 09:49:32",
+      "Submitted at": "6/3/2024 9:49",
       "title": "TMJ",
-      "longTitle": "TECHNOMEDIA JOURNAL",
+      "longTitle": "Technomedia Journal",
       "sinta": "SINTA 3",
       "program": "INFORMATIKA",
       "fee": 1500000,
       "urlJournal": "https://ijc.ilearning.co/index.php/TMJ/"
   },
   {
-      "Submitted at": "2024-06-03 10:00:27",
+      "Submitted at": "6/3/2024 10:00",
       "title": "OIKOS",
-      "longTitle": "Jurnal Kajian Pendidikan Ekonomi dan Ilmu Ekonomi",
+      "longTitle": "Jurnal Kajian Pendidikan Ekonomi Dan Ilmu Ekonomi",
       "sinta": "SINTA 4",
       "program": "MANAJEMEN, EKONOMI",
       "fee": 500000,
       "urlJournal": "https://journal.unpas.ac.id/index.php/oikos"
   },
   {
-      "Submitted at": "2024-06-03 10:08:28",
+      "Submitted at": "6/3/2024 10:08",
       "title": "JUTIF",
       "longTitle": "Jurnal Teknik Informatika",
       "sinta": "SINTA 3",
@@ -99,12 +99,201 @@ export const data = [
       "urlJournal": "http://jutif.if.unsoed.ac.id/index.php/jurnal"
   },
   {
-      "Submitted at": "2024-06-13 10:23:19",
+      "Submitted at": "6/13/2024 10:23",
       "title": "BK An-Nur",
-      "longTitle": "Jurnal Mahasiswa BK An-Nur : Berbeda, Bermakna, Mulia",
+      "longTitle": "Jurnal Mahasiswa Bk An-Nur : Berbeda, Bermakna, Mulia",
       "sinta": "SINTA 4",
       "program": "PSIKOLOGI",
       "fee": 350000,
       "urlJournal": "https://ojs.uniska-bjm.ac.id/index.php/AN-NUR/index"
+  },
+  {
+      "Submitted at": "6/14/2024 11:18",
+      "title": "IJoST",
+      "longTitle": "Indonesian Journal Of Science And Technology",
+      "sinta": "SINTA 1",
+      "program": "INFORMATIKA, MATEMATIKA, TEKNIK ELEKTRO, KIMIA, TEKNIK SIPIL, TEKNOLOGI PANGAN, FISIKA",
+      "fee": 8235875,
+      "urlJournal": "https://ejournal.upi.edu/index.php/ijost/index"
+  },
+  {
+      "Submitted at": "6/14/2024 11:21",
+      "title": "InfoTekJar",
+      "longTitle": "Jurnal Nasional Informatika Dan Teknologi Jaringan",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 500000,
+      "urlJournal": "https://jurnal.uisu.ac.id/index.php/infotekjar"
+  },
+  {
+      "Submitted at": "6/14/2024 11:24",
+      "title": "JPIT",
+      "longTitle": "Jurnal Pengembangan It",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 500000,
+      "urlJournal": "https://ejournal.poltekharber.ac.id/index.php/informatika"
+  },
+  {
+      "Submitted at": "6/14/2024 11:26",
+      "title": "SIMIKA",
+      "longTitle": "Jurnal Sistem Informasi Dan Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 300000,
+      "urlJournal": "https://ejournal.lppm-unbaja.ac.id/index.php/jsii/about"
+  },
+  {
+      "Submitted at": "6/14/2024 11:28",
+      "title": "JuTISI",
+      "longTitle": "Jurnal Teknik Informatika Dan Sistem Informasi",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 0,
+      "urlJournal": "https://journal.maranatha.edu/index.php/jutisi"
+  },
+  {
+      "Submitted at": "6/14/2024 11:31",
+      "title": "JBI",
+      "longTitle": "Jurnal Buana Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 0,
+      "urlJournal": "https://ojs.uajy.ac.id/index.php/jbi"
+  },
+  {
+      "Submitted at": "6/14/2024 11:33",
+      "title": "MIB Journal",
+      "longTitle": "Jurnal Media Informatika Budidarma",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 750000,
+      "urlJournal": "https://ejurnal.stmik-budidarma.ac.id/index.php/mib"
+  },
+  {
+      "Submitted at": "6/14/2024 11:34",
+      "title": "JINRPL",
+      "longTitle": "Jurnal Informatika Dan Rekayasa Perangkat Lunak",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 300000,
+      "urlJournal": "https://publikasiilmiah.unwahas.ac.id/index.php/JINRPL"
+  },
+  {
+      "Submitted at": "6/14/2024 11:43",
+      "title": "JISKA",
+      "longTitle": "Jurnal Informatika Sunan Kalijaga",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 0,
+      "urlJournal": "https://ejournal.uin-suka.ac.id/saintek/JISKA"
+  },
+  {
+      "Submitted at": "6/14/2024 11:45",
+      "title": "PETIR",
+      "longTitle": "Jurnal Pengkajian Dan Penerapan Teknik Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 0,
+      "urlJournal": "https://jurnal.itpln.ac.id/petir"
+  },
+  {
+      "Submitted at": "6/14/2024 11:46",
+      "title": "JTMI",
+      "longTitle": "Jurnal Teknologi Dan Manajemen Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 1000000,
+      "urlJournal": "https://jurnal.unmer.ac.id/index.php/jtmi/index"
+  },
+  {
+      "Submitted at": "6/14/2024 11:48",
+      "title": "JIPI",
+      "longTitle": "Jurnal Ilmiah Penelitian Dan Pembelajaran Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 600000,
+      "urlJournal": "https://jurnal.stkippgritulungagung.ac.id/index.php/jipi/index"
+  },
+  {
+      "Submitted at": "6/14/2024 11:49",
+      "title": "Edumatic",
+      "longTitle": "Jurnal Pendidikan Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 750000,
+      "urlJournal": "https://e-journal.hamzanwadi.ac.id/index.php/edumatic"
+  },
+  {
+      "Submitted at": "6/14/2024 11:51",
+      "title": "JIM",
+      "longTitle": "Jurnal Ilmiah Ilmu Komputer",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 500000,
+      "urlJournal": "https://e-journals.unmul.ac.id/index.php/JIM"
+  },
+  {
+      "Submitted at": "6/14/2024 11:55",
+      "title": "Telematika",
+      "longTitle": "Jurnal Informatika Dan Teknologi Informasi",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 0,
+      "urlJournal": "http://jurnal.upnyk.ac.id/index.php/telematika/index"
+  },
+  {
+      "Submitted at": "6/14/2024 11:56",
+      "title": "MATRIX",
+      "longTitle": "Jurnal Manajemen Teknologi Dan Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 500000,
+      "urlJournal": "https://ojs2.pnb.ac.id/index.php/matrix"
+  },
+  {
+      "Submitted at": "6/14/2024 12:19",
+      "title": "JUTIF",
+      "longTitle": "Jurnal Teknik Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 500000,
+      "urlJournal": "http://jutif.if.unsoed.ac.id/index.php/jurnal"
+  },
+  {
+      "Submitted at": "6/14/2024 12:24",
+      "title": "SinkrOn",
+      "longTitle": "Jurnal Dan Penelitian Teknik Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 1000000,
+      "urlJournal": "https://jurnal.polgan.ac.id/index.php/sinkron"
+  },
+  {
+      "Submitted at": "6/14/2024 12:33",
+      "title": "Krea-TIF",
+      "longTitle": "Jurnal Teknik Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 350000,
+      "urlJournal": "https://ejournal.uika-bogor.ac.id/index.php/krea-tif/index"
+  },
+  {
+      "Submitted at": "6/14/2024 12:43",
+      "title": "SATESI",
+      "longTitle": "Jurnal Sains Teknologi Dan Sistem Informasi",
+      "sinta": "SINTA 5",
+      "program": "INFORMATIKA",
+      "fee": 350000,
+      "urlJournal": "https://journal.yp3a.org/index.php/satesi"
+  },
+  {
+      "Submitted at": "6/14/2024 12:44",
+      "title": "JIKA",
+      "longTitle": "Jurnal Ilmu Komputer Dan Agri-Informatika",
+      "sinta": "SINTA 3",
+      "program": "INFORMATIKA",
+      "fee": 750000,
+      "urlJournal": "https://journal.ipb.ac.id/index.php/jika/index"
   }
 ]
